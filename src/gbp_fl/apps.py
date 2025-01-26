475aa10eaecd83971d53221e86cff057cc195252
Albert Hopkins <marduk@letterboxes.org>
2025-01-26 07:28:45 -0600

    tests: add test infrastructure
    No tests but the infrastructure is there.

---
+++ src/gbp_fl/apps.py
@@ -0,0 +1,11 @@
+"""AppsConfig for gbp-fl"""
+
+from django.apps import AppConfig
+
+
+class GBPFLConfig(AppConfig):
+    """AppConfig for gbp-fl"""
+
+    name = "gbp_fl"
+    verbose_name = "File Lists for Gentoo Build Publisher"
+    default_auto_field = "django.db.models.BigAutoField"
