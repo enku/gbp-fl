2c241c7c0690207d2a0dfe1da17ba55cf41f5e25
Albert Hopkins <marduk@letterboxes.org>
2025-09-13 12:37:15 -0500

    ui: add files info to the machine details page
    Inject some files data (total number of files, average files per build)
    into the machine details page.

--- src/gbp_fl/django/gbp_fl/templatetags/gbp_fl.py
+++ src/gbp_fl/django/gbp_fl/templatetags/gbp_fl.py
@@ -24,3 +24,21 @@ def file_count() -> int:
     cache.set("file-count", count, timeout=1800)
 
     return count
+
+
+@register.simple_tag
+def machine_file_count(machine: str, build_count: int) -> dict[str, int]:
+    """Return the total and average file counts for the given machine
+
+    Returns a dict like:
+
+        {
+            "total": 50000,      # total count of files for the machine
+            "per_build": 10000,  # average number of files per machine's builds
+        }
+    """
+    repo = Repo.from_settings(Settings.from_environ())
+    count = repo.files.count(  # pylint: disable=assignment-from-no-return
+        machine, None, None
+    )
+    return {"total": count, "per_build": count // build_count}
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -4,8 +4,11 @@
 
 from unittest import TestCase
 
+import gbp_testkit.fixtures as testkit
 from django.core.cache import cache
-from unittest_fixtures import Fixtures, given
+from gentoo_build_publisher import publisher
+from gentoo_build_publisher.types import Build as GBPBuild
+from unittest_fixtures import Fixtures, given, where
 
 from gbp_fl.django.gbp_fl.templatetags.gbp_fl import file_count
 
@@ -30,3 +33,34 @@ class FileCountTest(TestCase):
         result = file_count()
 
         self.assertEqual(result, 30_000)
+
+
+CONTENTS = """
+    polaris 26 app-arch/tar-1.35-1       /bin/gtar
+    polaris 26 app-shells/bash-5.2_p37-1 /bin/bash
+    polaris 26 app-shells/bash-5.2_p37-2 /bin/bash
+    polaris 27 app-shells/bash-5.2_p37-1 /bin/bash
+"""
+
+
+@given(testkit.publisher, lib.bulk_content_files, build1=lib.build, build2=lib.build)
+@given(lib.repo, testkit.client)
+@where(bulk_content_files=CONTENTS)
+@where(build1__build="polaris.26")
+@where(build2__build="polaris.27")
+class MachineDetaiViewTests(TestCase):
+    def test(self, fixtures: Fixtures) -> None:
+        publisher.pull(GBPBuild(machine="polaris", build_id="26"))
+        publisher.pull(GBPBuild(machine="polaris", build_id="27"))
+        repo = fixtures.repo
+        repo.files.bulk_save(fixtures.bulk_content_files)
+
+        response = fixtures.client.get("/machines/polaris/")
+
+        expected = 'Files <span class="badge badge-primary badge-pill">4</span>'
+        self.assertIn(expected, response.text)
+
+        expected = (
+            'Files per build <span class="badge badge-primary badge-pill">2</span>'
+        )
+        self.assertIn(expected, response.text)
