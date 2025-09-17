ddefe76b0a09289983a5c32fac5c6ea1efdb9353
Albert Hopkins <marduk@letterboxes.org>
2025-09-17 14:48:38 -0500

    add plugin checks
    Since the builds and file records have the possibility of getting out of
    sync, this utilizes the new "plugin checks" feature of Gentoo Build
    Publisher to add to checks: one to ensure that every GBP Build has file
    records and one to ensure that every file record has a corresponding GBP
    Build. These checks return warnings instead of errors because when there
    is an inconsistency it's not fatal.

--- src/gbp_fl/__init__.py
+++ src/gbp_fl/__init__.py
@@ -15,4 +15,8 @@ plugin = {
     "version": __version__,
     "graphql": "gbp_fl.graphql",
     "priority": -10,
+    "checks": {
+        "all_builds_have_indices": "gbp_fl.checks:all_builds_have_indices",
+        "all_indices_have_builds": "gbp_fl.checks:all_indices_have_builds",
+    },
 }
