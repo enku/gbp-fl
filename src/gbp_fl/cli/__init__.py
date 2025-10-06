0dfa90f38f09dc8922ebd01633228ef17b53b727
Albert Hopkins <marduk@letterboxes.org>
2025-10-06 06:19:36 -0500

    cli: extract out subparser creation to its own function
    Extract out the loop body and make it a function. We are going to want
    to use this later to implement server-only subcommands.

--- src/gbp_fl/cli/__init__.py
+++ src/gbp_fl/cli/__init__.py
@@ -7,13 +7,20 @@ there is nothing (yet) to do at the "root" level.
 import argparse
 import functools
 import importlib
-from typing import IO
+from typing import IO, Any, Protocol
 
 from gbpcli.gbp import GBP
 from gbpcli.types import Console
 
 HELP = "Access the Gentoo Build Publisher File List"
 SUBCOMMANDS = ("fetch", "ls", "search", "stats")
+
+
+# subpparsers' type appear to be undocumented/unpublished so we spec out as much as we
+# need here.
+class SubParsersProtocol(Protocol):
+    # pylint: disable=missing-docstring,too-few-public-methods
+    def add_parser(self, name: str, **kwargs: Any) -> argparse.ArgumentParser: ...
 
 
 def handler(args: argparse.Namespace, _gbp: GBP, console: Console) -> int:
@@ -27,11 +34,17 @@ def parse_args(parser: argparse.ArgumentParser) -> None:
     subparsers = parser.add_subparsers()
 
     for subcommand in SUBCOMMANDS:
+        add_subparser(subcommand, subparsers)
 
-        module = importlib.import_module(f"gbp_fl.cli.{subcommand}")
-        subparser = subparsers.add_parser(subcommand, help=module.HELP)
-        module.parse_args(subparser)
-        subparser.set_defaults(func=getattr(module, "handler"))
+
+def add_subparser(name: str, subparsers: SubParsersProtocol) -> argparse.ArgumentParser:
+    """Create a subparser for the given subcommand"""
+    module = importlib.import_module(f"gbp_fl.cli.{name}")
+    subparser = subparsers.add_parser(name, help=module.HELP)
+    module.parse_args(subparser)
+    subparser.set_defaults(func=getattr(module, "handler"))
+
+    return subparser
 
 
 def print_usage(io: IO[str]) -> None:
